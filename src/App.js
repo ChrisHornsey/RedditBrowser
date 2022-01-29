acf644526bad275524f2d9f481658e5f28ee4c7e
Chris Hornsey <chris@thehornseys.com>
2022-01-29 14:48:03 +0000

    Fixing test, and not specifying node version

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          shmearn React
+          Learn React
         </a>
       </header>
     </div>
