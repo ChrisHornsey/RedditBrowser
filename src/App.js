fc109ea476f274cf4b6c340f18204caba97011b4
Chris Hornsey <chris@thehornseys.com>
2022-01-29 14:43:27 +0000

    breaking tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          shmearn React
         </a>
       </header>
     </div>
