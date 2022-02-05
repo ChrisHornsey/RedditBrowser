19c5998e2d40892520500b22d6d0169a2e7b9d7b
Chris Hornsey <chris@thehornseys.com>
2022-02-05 11:12:30 +0000

    finished off helpers unit test

--- src/app/helpers.test.js
+++ src/app/helpers.test.js
@@ -10,3 +10,16 @@ it('ensures that the titles are text', () => {
     const title = tidiedData[0].title
     assert.ok(typeof(title) === "string")
 })
+it('ensures that the ids are unique', () => {
+    const tidiedData = postDataTidier(ExamplePostList);
+
+    const ids = tidiedData.map(post => post.id);
+    assert.ok(new Set(ids).size === ids.length);
+})
+it('updates the image URL to where images can be accessed', () => {
+
+    const tidiedData = postDataTidier(ExamplePostList);
+
+    const image = tidiedData[2].image;
+    assert.ok(image.includes("https://i.redd.it/"));
+})
